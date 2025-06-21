d51d5f2e4551ee1f84f3894d3a074704bce7d8c1
EC2 Default User <ec2-user@ip-172-31-2-165.us-east-2.compute.internal>
2025-06-21 17:31:48 +0000

    added terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.ass_15_sg1.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  from_port         = 443
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
